41ac1ada1385c75299aabe3ebe35dc30e99d1904
caua-labes <caua_labes@estudante.sesisenai.org.br>
2023-10-22 01:36:40 -0300

    Melhora do css e uma pequena base script diferente

--- fisica.js
+++ fisica.js
@@ -1,190 +1,63 @@
-var jogador = "J1";
+jogador1 = false
+jogador2 = false
+jogador3 = false
+jogador4 = false
 
-var pJ1 = 0;
+var pj1 = 0
+var pj2 = 0
+var pj3 = 0
+var pj4 = 0
 
-var pJ2 = 0;
-
-var pJ3 = 0;
-
-var pJ4 = 0;
-
-var pAtual1 = "";
-
-var pAtual2 = "";
-
-var pAtual3 = "";
-
-var pAtual4 = "";
-
-document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
-
-var tabLength = document.querySelectorAll("div").length - 1;
-
-function dado() {
-    var resultado = Math.floor(Math.random() * 6);
-    if (resultado == 0) {
-        resultado = resultado + 1;
+function rodar() {
+    var posicao = parseInt(Math.random() * 6)
+    if (posicao == 0) {
+        posicao = 6
     }
-
-    console.log(resultado)
-
-    if (jogador == "J1") {
-        if (document.getElementsByTagName("p")[1].innerHTML == jogador) {
-            pAtual1 = document.getElementsByTagName("p")[1];
-        } else if (document.getElementsByTagName("p")[2].innerHTML == jogador) {
-            pAtual1 = document.getElementsByTagName("p")[2];
-        } else if (document.getElementsByTagName("p")[3].innerHTML == jogador) {
-            pAtual1 = document.getElementsByTagName("p")[3];
-        } else {
-            pAtual1 = document.getElementsByTagName("p")[0];
+    if (jogador1 == false) {
+        if (pj1 == 0) {
+            document.getElementsByTagName("div")[pj1 + 2].innerHTML = ""
+            pj1 = 2
         }
-
-        pAtual1.remove();
-
-        pJ1 = (pJ1 + resultado) % tabLength;
-
-        document.getElementsByTagName("div")[pJ1 + 1].innerHTML += ` <p class="p-2 text-success">${jogador}</p> `;
-
-        if (document.getElementsByTagName("div")[pJ1 + 1].classList.contains("bg-dark")) {
-            document.body.insertAdjacentHTML("beforeend", `<div class="pergunta">
-                                                            <p class="w-100 p-3 bg-primary text-center perguntaTitulo">Pergunta para o jogador ${jogador}</p>
-                                                    
-                                                            <p style="position: absolute; top: 12%; font-weight: 750; word-break: break-all;" class="m-2">Texto da Pergunta</p>
-                                                    
-                                                            <p style="position: absolute; top: 38%; left: 4%; font-weight: 750;">Alternativa 1</p>
-                                                            <p style="position: absolute; top: 48%; left: 4%; font-weight: 750;">Alternativa 2</p>
-                                                            <p style="position: absolute; top: 58%; left: 4%; font-weight: 750;">Alternativa 3</p>
-                                                    
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 50px; width: 10%; left: 15%; top: 75%;">A</button>
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 30px; width: 10%;  left: 45%; top: 75%;">B</button>
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 30px; width: 10%; left: 75%; top: 75%;">C</button>
-                                                        </div>`);
-
-            document.getElementById("jogar").setAttribute("disabled");
-        }
-
-        jogador = "J2";
-
-        document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
-    } else if (jogador == "J2") {
-        if (document.getElementsByTagName("p")[1].innerHTML == jogador) {
-            pAtual2 = document.getElementsByTagName("p")[1];
-        } else if (document.getElementsByTagName("p")[2].innerHTML == jogador) {
-            pAtual2 = document.getElementsByTagName("p")[2];
-        } else if (document.getElementsByTagName("p")[3].innerHTML == jogador) {
-            pAtual2 = document.getElementsByTagName("p")[3];
-        } else {
-            pAtual2 = document.getElementsByTagName("p")[0];
-        }
-
-        pAtual2.remove();
-
-        pJ2 = (pJ2 + resultado) % tabLength;
-
-        document.getElementsByTagName("div")[pJ2 + 1].innerHTML += ` <p class="p-2 text-primary">${jogador}</p> `;
-
-        if (document.getElementsByTagName("div")[pJ2 + 1].classList.contains("bg-dark")) {
-            document.body.insertAdjacentHTML("beforeend", `<div class="pergunta">
-                                                            <p class="w-100 p-3 bg-primary text-center perguntaTitulo">Pergunta para o jogador ${jogador}</p>
-                                                    
-                                                            <p style="position: absolute; top: 12%; font-weight: 750; word-break: break-all;" class="m-2">Texto da Pergunta</p>
-                                                    
-                                                            <p style="position: absolute; top: 38%; left: 4%; font-weight: 750;">Alternativa 1</p>
-                                                            <p style="position: absolute; top: 48%; left: 4%; font-weight: 750;">Alternativa 2</p>
-                                                            <p style="position: absolute; top: 58%; left: 4%; font-weight: 750;">Alternativa 3</p>
-                                                    
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 50px; width: 10%; left: 15%; top: 75%;">A</button>
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 30px; width: 10%;  left: 45%; top: 75%;">B</button>
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 30px; width: 10%; left: 75%; top: 75%;">C</button>
-                                                        </div>`);
-
-            document.getElementById("jogar").setAttribute("disabled");
-        }
-
-        jogador = "J3";
-
-        document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
+        document.getElementsByTagName("div")[pj1].innerHTML = ""
+        pj1 = pj1 + posicao + 1
+        document.getElementsByTagName("div")[pj1].innerHTML += `<p>J1</p>`
+        jogador1 = true
+        jogador2 = false
+        console.log(posicao)
+        console.log(pj1)
     }
-
-    else if (jogador == "J3") {
-        if (document.getElementsByTagName("p")[1].innerHTML == jogador) {
-            pAtual3 = document.getElementsByTagName("p")[1];
-        } else if (document.getElementsByTagName("p")[2].innerHTML == jogador) {
-            pAtual3 = document.getElementsByTagName("p")[2];
-        } else if (document.getElementsByTagName("p")[3].innerHTML == jogador) {
-            pAtual3 = document.getElementsByTagName("p")[3];
-        } else {
-            pAtual3 = document.getElementsByTagName("p")[0];
+    else if (jogador2 == false) {
+        if (pj2 == 0) {
+            document.getElementsByTagName("div")[pj2 + 2].innerHTML = ""
+            pj2 = 2
         }
-
-        pAtual3.remove();
-
-        pJ3 = (pJ3 + resultado) % tabLength;
-
-        document.getElementsByTagName("div")[pJ3 + 1].innerHTML += ` <p class="p-2 text-danger">${jogador}</p> `;
-
-        if (document.getElementsByTagName("div")[pJ3 + 1].classList.contains("bg-dark")) {
-            document.body.insertAdjacentHTML("beforeend", `<div class="pergunta">
-                                                            <p class="w-100 p-3 bg-primary text-center perguntaTitulo">Pergunta para o jogador ${jogador}</p>
-                                                    
-                                                            <p style="position: absolute; top: 12%; font-weight: 750; word-break: break-all;" class="m-2">Texto da Pergunta</p>
-                                                    
-                                                            <p style="position: absolute; top: 38%; left: 4%; font-weight: 750;">Alternativa 1</p>
-                                                            <p style="position: absolute; top: 48%; left: 4%; font-weight: 750;">Alternativa 2</p>
-                                                            <p style="position: absolute; top: 58%; left: 4%; font-weight: 750;">Alternativa 3</p>
-                                                    
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 50px; width: 10%; left: 15%; top: 75%;">A</button>
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 30px; width: 10%;  left: 45%; top: 75%;">B</button>
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 30px; width: 10%; left: 75%; top: 75%;">C</button>
-                                                        </div>`);
-
-            document.getElementById("jogar").setAttribute("disabled");
-        }
-
-        jogador = "J4";
-
-        document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
-    } else {
-        if (document.getElementsByTagName("p")[1].innerHTML == jogador) {
-            pAtual4 = document.getElementsByTagName("p")[1];
-        } else if (document.getElementsByTagName("p")[2].innerHTML == jogador) {
-            pAtual4 = document.getElementsByTagName("p")[2];
-        } else if (document.getElementsByTagName("p")[3].innerHTML == jogador) {
-            pAtual4 = document.getElementsByTagName("p")[3];
-        } else {
-            pAtual4 = document.getElementsByTagName("p")[0];
-        }
-
-        pAtual4.remove();
-
-        pJ4 = (pJ4 + resultado) % tabLength;
-
-        document.getElementsByTagName("div")[pJ4 + 1].innerHTML += ` <p class="p-2 text-dark">${jogador}</p> `;
-
-        if (document.getElementsByTagName("div")[pJ4 + 1].classList.contains("bg-dark")) {
-            document.body.insertAdjacentHTML("beforeend", `<div class="pergunta">
-                                                            <p class="w-100 p-3 bg-primary text-center perguntaTitulo">Pergunta para o jogador ${jogador}</p>
-                                                    
-                                                            <p style="position: absolute; top: 12%; font-weight: 750; word-break: break-all;" class="m-2">Texto da Pergunta</p>
-                                                    
-                                                            <p style="position: absolute; top: 38%; left: 4%; font-weight: 750;">Alternativa 1</p>
-                                                            <p style="position: absolute; top: 48%; left: 4%; font-weight: 750;">Alternativa 2</p>
-                                                            <p style="position: absolute; top: 58%; left: 4%; font-weight: 750;">Alternativa 3</p>
-                                                    
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 50px; width: 10%; left: 15%; top: 75%;">A</button>
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 30px; width: 10%;  left: 45%; top: 75%;">B</button>
-                                                            <button onclick="responder()" style="position: absolute; border-radius: 30px; width: 10%; left: 75%; top: 75%;">C</button>
-                                                        </div>`);
-
-            document.getElementById("jogar").setAttribute("disabled");
-        }
-
-        jogador = "J1";
-
-        document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
+        document.getElementsByTagName("div")[pj2].innerHTML = ""
+        pj2 = pj2 + posicao + 1
+        document.getElementsByTagName("div")[pj2].innerHTML += `<p>J2</p>`
+        jogador2 = true
+        jogador3 = false
     }
-}
-
-function responder() {
+    else if (jogador3 == false) {
+        if (pj3 == 0) {
+            document.getElementsByTagName("div")[pj3 + 2].innerHTML = ""
+            pj3 = 2
+        }
+        document.getElementsByTagName("div")[pj3].innerHTML = ""
+        pj3 = pj3 + posicao + 1
+        document.getElementsByTagName("div")[pj3].innerHTML += `<p>J3</p>`
+        jogador3 = true
+        jogador4 = false
+    }
+    else if (jogador4 == false) {
+        if (pj4 == 0) {
+            document.getElementsByTagName("div")[pj4 + 2].innerHTML = ""
+            pj4 = 2
+        }
+        document.getElementsByTagName("div")[pj4].innerHTML = ""
+        pj4 = pj4 + posicao + 1
+        document.getElementsByTagName("div")[pj4].innerHTML += `<p>J4</p>`
+        jogador4 = true
+        jogador1 = false
+    }
 
 }
