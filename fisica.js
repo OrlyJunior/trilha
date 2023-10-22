7ff9975a9f9ee01e8a1f9c71f18076feb8ee0a45
unknown <orlyjunior21@gmail.com>
2023-10-22 17:48:23 -0300

    Poder 'Um ano em Plutão'(bloquear um jogador) iniciado mas não totalmente funcional ainda.

--- fisica.js
+++ fisica.js
@@ -1,5 +1,15 @@
 var jogador = "J1";
 
+var block1 = false;
+
+var block1 = false;
+
+var block2 = false;
+
+var block3 = false;
+
+var block4 = false;
+
 var pJ1 = 0;
 
 var pJ2 = 0;
@@ -18,9 +28,9 @@ var pAtual4 = "";
 
 var j1P1 = false;
 
-var j1P2 = true;
+var j1P2 = false;
 
-var j1P3 = false;
+var j1P3 = true;
 
 var j2P1 = false;
 
@@ -202,7 +212,7 @@ function correto() {
 function continuar() {
     document.getElementById("janela").remove();
 
-    if(jogador == "J1"){
+    if (jogador == "J1") {
         if (j2P1 == true) {
             document.body.insertAdjacentHTML("beforeend", `<div id="fora" onclick="foraOrbita()" class="poder"><div class="poder2"><img src="Imagens/645410.png" width="85%"></div></div>`);
         }
@@ -220,7 +230,7 @@ function continuar() {
         document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
     }
 
-    if(jogador == "J2"){
+    if (jogador == "J2") {
         if (j3P1 == true) {
             document.body.insertAdjacentHTML("beforeend", `<div id="fora" onclick="foraOrbita()" class="poder"><div class="poder2"><img src="Imagens/645410.png" width="85%"></div></div>`);
         }
@@ -238,7 +248,7 @@ function continuar() {
         document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
     }
 
-    if(jogador == "J3"){
+    if (jogador == "J3") {
         if (j4P1 == true) {
             document.body.insertAdjacentHTML("beforeend", `<div id="fora" onclick="foraOrbita()" class="poder"><div class="poder2"><img src="Imagens/645410.png" width="85%"></div></div>`);
         }
@@ -256,7 +266,7 @@ function continuar() {
         document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
     }
 
-    if(jogador == "J4"){
+    if (jogador == "J4") {
         if (j1P1 == true) {
             document.body.insertAdjacentHTML("beforeend", `<div id="fora" onclick="foraOrbita()" class="poder"><div class="poder2"><img src="Imagens/645410.png" width="85%"></div></div>`);
         }
@@ -311,7 +321,7 @@ function errado() {
 }
 
 function dado() {
-    console.log(Math.floor(Math.random() * 3));
+    console.log(block1);
 
     var resultado = Math.floor(Math.random() * 6);
     if (resultado == 0) {
@@ -319,6 +329,18 @@ function dado() {
     }
 
     if (jogador == "J1") {
+        if (block1 == true) {
+            document.body.insertAdjacentHTML("beforeend", `<div style="height: 35%; top: 30%;" id="janela" class="pergunta">
+                                                                <p class="w-100 p-3 bg-primary text-center perguntaTitulo">O jogador J1 está bloqueado!</p>
+                                                        
+                                                                <button onclick="continuar()" style="position: absolute; border-radius: 30px; width: 30%; left: 36%; top: 45%;">Continuar</button>
+                                                            </div>`);
+
+            block1 = false;
+
+            return;
+        }
+
         if (document.getElementsByTagName("p")[1].innerHTML == jogador) {
             pAtual1 = document.getElementsByTagName("p")[1];
         } else if (document.getElementsByTagName("p")[2].innerHTML == jogador) {
@@ -335,29 +357,7 @@ function dado() {
 
         document.getElementsByTagName("div")[pJ1 + 1].innerHTML += ` <p class="p-2 text-success">${jogador}</p> `;
 
-        if (document.getElementById("fora")) {
-            document.getElementById("fora").remove();
-        }
 
-        if (document.getElementById("mercurio")) {
-            document.getElementById("mercurio").remove();
-        }
-
-        if (document.getElementById("plutao")) {
-            document.getElementById("plutao").remove();
-        }
-
-        if (j2P1 == true) {
-            document.body.insertAdjacentHTML("beforeend", `<div id="fora" onclick="foraOrbita()" class="poder"><div class="poder2"><img src="Imagens/645410.png" width="85%"></div></div>`);
-        }
-
-        if (j2P2 == true) {
-            document.body.insertAdjacentHTML("beforeend", `<div id="mercurio" onclick="mercurio()" class="poder"><div class="poder2"><img src="Imagens/mercurio_318-881959.png" width="85%"></div></div>`);
-        }
-
-        if (j2P3 == true) {
-            document.body.insertAdjacentHTML("beforeend", `<div id="plutao" onclick="plutao" class="poder"><div class="poder2"><img src="Imagens/pngtree-pluto-line-icon-vector-png-image_6640934.png" width="85%"></div></div>`)
-        }
 
 
 
@@ -384,6 +384,18 @@ function dado() {
 
         document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
     } else if (jogador == "J2") {
+        if (block2 == true) {
+            document.body.insertAdjacentHTML("beforeend", `<div style="height: 35%; top: 30%;" id="janela" class="pergunta">
+                                                                <p class="w-100 p-3 bg-primary text-center perguntaTitulo">O jogador J2 está bloqueado!</p>
+                                                        
+                                                                <button onclick="continuar()" style="position: absolute; border-radius: 30px; width: 30%; left: 36%; top: 45%;">Continuar</button>
+                                                            </div>`);
+
+            block2 = false;
+
+            return;
+        }
+
         if (document.getElementsByTagName("p")[1].innerHTML == jogador) {
             pAtual2 = document.getElementsByTagName("p")[1];
         } else if (document.getElementsByTagName("p")[2].innerHTML == jogador) {
@@ -452,6 +464,18 @@ function dado() {
     }
 
     else if (jogador == "J3") {
+        if (block3 == true) {
+            document.body.insertAdjacentHTML("beforeend", `<div style="height: 35%; top: 30%;" id="janela" class="pergunta">
+                                                                <p class="w-100 p-3 bg-primary text-center perguntaTitulo">O jogador J2 está bloqueado!</p>
+                                                        
+                                                                <button onclick="continuar()" style="position: absolute; border-radius: 30px; width: 30%; left: 36%; top: 45%;">Continuar</button>
+                                                            </div>`);
+
+            block3 = false;
+
+            return;
+        }
+
         if (document.getElementsByTagName("p")[1].innerHTML == jogador) {
             pAtual3 = document.getElementsByTagName("p")[1];
         } else if (document.getElementsByTagName("p")[2].innerHTML == jogador) {
@@ -518,6 +542,18 @@ function dado() {
 
         document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
     } else if (jogador == "J4") {
+        if (block4 == true) {
+            document.body.insertAdjacentHTML("beforeend", `<div style="height: 35%; top: 30%;" id="janela" class="pergunta">
+                                                                <p class="w-100 p-3 bg-primary text-center perguntaTitulo">O jogador J2 está bloqueado!</p>
+                                                        
+                                                                <button onclick="continuar()" style="position: absolute; border-radius: 30px; width: 30%; left: 36%; top: 45%;">Continuar</button>
+                                                            </div>`);
+
+            block4 = false;
+
+            return;
+        }
+
         if (document.getElementsByTagName("p")[1].innerHTML == jogador) {
             pAtual4 = document.getElementsByTagName("p")[1];
         } else if (document.getElementsByTagName("p")[2].innerHTML == jogador) {
@@ -870,5 +906,91 @@ function mercurio() {
         document.getElementById("mercurio").remove();
 
         return;
+    }
+}
+
+function plutao() {
+    if (jogador == "J1") {
+        document.body.insertAdjacentHTML("beforeend", `<div id="volta" style="height: 30%; top: 25%;" class="pergunta">
+                                                        <p class="w-100 p-3 bg-primary text-center perguntaTitulo">Qual jogador você deseja que fique uma rodada sem jogar?</p>
+
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 50px; width: 10%; left: 15%; top: 45%;">J2</button>
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 30px; width: 10%;  left: 45%; top: 45%;">J3</button>
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 30px; width: 10%; left: 75%; top: 45%;">J4</button>
+                                                      </div>`);
+
+        j1P3 = false;
+
+        document.getElementById("plutao").remove();
+
+        return;
+    }
+
+    if (jogador == "J2") {
+        document.body.insertAdjacentHTML("beforeend", `<div id="volta" style="height: 30%; top: 25%;" class="pergunta">
+                                                        <p class="w-100 p-3 bg-primary text-center perguntaTitulo">Qual jogador você deseja que fique uma rodada sem jogar?</p>
+
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 50px; width: 10%; left: 15%; top: 45%;">J1</button>
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 30px; width: 10%;  left: 45%; top: 45%;">J3</button>
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 30px; width: 10%; left: 75%; top: 45%;">J4</button>
+                                                      </div>`);
+
+        j2P3 = false;
+
+        document.getElementById("plutao").remove();
+
+        return;
+    }
+
+    if (jogador == "J3") {
+        document.body.insertAdjacentHTML("beforeend", `<div id="volta" style="height: 30%; top: 25%;" class="pergunta">
+                                                        <p class="w-100 p-3 bg-primary text-center perguntaTitulo">Qual jogador você deseja que fique uma rodada sem jogar?</p>
+
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 50px; width: 10%; left: 15%; top: 45%;">J1</button>
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 30px; width: 10%;  left: 45%; top: 45%;">J2</button>
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 30px; width: 10%; left: 75%; top: 45%;">J4</button>
+                                                      </div>`);
+
+        j3P3 = false;
+
+        document.getElementById("plutao").remove();
+
+        return;
+    }
+
+    if (jogador == "J4") {
+        document.body.insertAdjacentHTML("beforeend", `<div id="volta" style="height: 30%; top: 25%;" class="pergunta">
+                                                        <p class="w-100 p-3 bg-primary text-center perguntaTitulo">Qual jogador você deseja que fique uma rodada sem jogar?</p>
+
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 50px; width: 10%; left: 15%; top: 45%;">J1</button>
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 30px; width: 10%;  left: 45%; top: 45%;">J2</button>
+                                                        <button onclick="bloqueia(this)" style="position: absolute; border-radius: 30px; width: 10%; left: 75%; top: 45%;">J3</button>
+                                                    </div>`);
+
+        j4P3 = false;
+
+        document.getElementById("plutao").remove();
+
+        return;
+    }
+}
+
+function bloqueia(player) {
+    console.log(player.innerHTML)
+
+    if (player.innerHTML == "J1") {
+        block1 = true;
+    }
+
+    if (player.innerHTML == "J2") {
+        block2 = true;
+    }
+
+    if (player.innerHTML == "J3") {
+        block3 = true;
+    }
+
+    if (player.innerHTML == "J4") {
+        block4 = true;
     }
 }
