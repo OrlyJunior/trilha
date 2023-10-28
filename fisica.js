f989f48305becccf030f51db452d304ffbc60a53
unknown <orlyjunior21@gmail.com>
2023-10-28 20:45:07 -0300

    Mudança no JS, agora utilizando geteElementsByClassName ao invés de getElementsByTagName.

--- fisica.js
+++ fisica.js
@@ -16,11 +16,14 @@ var pAtual3 = "";
 
 var pAtual4 = "";
 
+
+
 document.getElementById("vez").innerHTML = `Vez de: ${jogador}`;
 
-var tabLength = document.querySelectorAll("div").length - 2;
+var tabLength = document.querySelectorAll("div.div").length - 1;
 
-console.log(document.getElementById("final").innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) 
+console.log(tabLength)
+
 
 function correto() {
     if (jogador == "J1") {
@@ -36,9 +39,9 @@ function correto() {
 
         pAtual1.remove();
 
-        pJ1 = (pJ1 + 2) % tabLength;
+        pJ1 = (pJ1 + 4) % tabLength;
 
-        document.getElementsByTagName("div")[pJ1 + 1].innerHTML += ` <p class="p-2 text-success">${jogador}</p> `;
+        document.getElementsByClassName("div")[pJ1].innerHTML += ` <p class="p-2 text-success">${jogador}</p> `;
 
         jogador = "J2";
 
@@ -64,9 +67,9 @@ function correto() {
 
         pAtual2.remove();
 
-        pJ2 = (pJ2 + 2) % tabLength;
+        pJ2 = (pJ2 + 4) % tabLength;
 
-        document.getElementsByTagName("div")[pJ2 + 1].innerHTML += ` <p class="p-2 text-primary">${jogador}</p> `;
+        document.getElementsByClassName("div")[pJ2].innerHTML += ` <p class="p-2 text-primary">${jogador}</p> `;
 
         jogador = "J3";
 
@@ -92,9 +95,9 @@ function correto() {
 
         pAtual3.remove();
 
-        pJ3 = (pJ2 + 3) % tabLength;
+        pJ3 = (pJ3 + 4) % tabLength;
 
-        document.getElementsByTagName("div")[pJ3 + 1].innerHTML += ` <p class="p-2 text-danger">${jogador}</p> `;
+        document.getElementsByClassName("div")[pJ3].innerHTML += ` <p class="p-2 text-danger">${jogador}</p> `;
 
         jogador = "J4";
 
@@ -120,9 +123,9 @@ function correto() {
 
         pAtual4.remove();
 
-        pJ4 = (pJ4 + 2) % tabLength;
+        pJ4 = (pJ4 + 4) % tabLength;
 
-        document.getElementsByTagName("div")[pJ4 + 1].innerHTML += ` <p class="p-2 text-warning">${jogador}</p> `;
+        document.getElementsByClassName("div")[pJ4].innerHTML += ` <p class="p-2 text-warning">${jogador}</p> `;
 
         jogador = "J1";
 
@@ -139,126 +142,140 @@ function correto() {
 function dado() {
     var resultado = Math.floor(Math.random() * 6);
 
-    if (document.getElementsByTagName("div")[40].innerHTML == ` <p class="text-success p-2 w-50">J1</p>` ) {
-        if(resultado > 1){
+    console.log(resultado)
+    console.log(document.getElementsByClassName("div")[21].innerHTML == ` <p class="text-success p-2 w-50">J1</p> `)
+    console.log(document.getElementsByClassName("div")[20].innerHTML == ` <p class="text-success p-2 w-50">J1</p> `)
+    console.log(document.getElementsByClassName("div")[19].innerHTML == ` <p class="text-success p-2 w-50">J1</p> `)
+    console.log(document.getElementsByClassName("div")[18].innerHTML == ` <p class="text-success p-2 w-50">J1</p> `)
+    console.log(document.getElementsByClassName("div")[17].innerHTML == ` <p class="text-success p-2 w-50">J1</p> `)
+    console.log(document.getElementsByClassName("div")[21].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `)
+    console.log(document.getElementsByClassName("div")[20].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `)
+    console.log(document.getElementsByClassName("div")[19].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `)
+    console.log(document.getElementsByClassName("div")[18].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `)
+    console.log(document.getElementsByClassName("div")[17].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `)
+
+    if(jogador == "J1"){
+        if (document.getElementById("um").innerHTML == ` <p class="text-success p-2 w-50">J1</p> `) {
             resultado = 1;
         }
+    
+        if (document.getElementById("dois").innerHTML == ` <p class="text-success p-2 w-50">J1</p> `) {
+            if (resultado > 2) {
+                resultado = 2;
+            }
+        }
+    
+        if (document.getElementById("tres").innerHTML == ` <p class="text-success p-2 w-50">J1</p> `) {
+            if (resultado > 3) {
+                resultado = 3;
+            }
+        }
+    
+        if (document.getElementById("quatro").innerHTML == ` <p class="text-success p-2 w-50">J1</p> `) {
+            if (resultado > 4) {
+                resultado = 4;
+            }
+        }
+    
+        if (document.getElementById("cinco").innerHTML == ` <p class="text-success p-2 w-50">J1</p> `) {
+            if (resultado > 5) {
+                resultado = 5;
+            }
+        }
+    
+        
     }
-
-    if (document.getElementsByTagName("div")[39].innerHTML == ` <p class="text-success p-2 w-50">J1</p> `) {
-        if(resultado > 2){
-            resultado = 2;
+    
+    if(jogador == "J2"){
+        if (document.getElementById("um").innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
+                resultado = 1;
+        }
+    
+        if (document.getElementById("dois").innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
+            if (resultado > 2) {
+                resultado = 2;
+            }
+        }
+    
+        if (document.getElementById("tres").innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
+            if (resultado > 3) {
+                resultado = 3;
+            }
+        }
+    
+        if (document.getElementById("quatro").innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
+            if (resultado > 4) {
+                resultado = 4;
+            }
+        }
+    
+        if (document.getElementById("cinco").innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
+            if (resultado > 5) {
+                resultado = 5;
+            }
         }
     }
 
-    if (document.getElementsByTagName("div")[38].innerHTML == ` <p class="text-success p-2 w-50">J1</p> `) {
-        if(resultado > 3){
-            resultado = 3;
+    if(jogador == "J3"){
+        if (document.getElementById("um").innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
+                resultado = 1;
+        }
+    
+        if (document.getElementById("dois").innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
+            if (resultado > 2) {
+                resultado = 2;
+            }
+        }
+    
+        if (document.getElementById("tres").innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
+            if (resultado > 3) {
+                resultado = 3;
+            }
+        }
+    
+        if (document.getElementById("quatro").innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
+            if (resultado > 4) {
+                resultado = 4;
+            }
+        }
+
+        if (document.getElementById("cinco").innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
+            if (resultado > 5) {
+                resultado = 5;
+            }
         }
     }
 
-    if (document.getElementsByTagName("div")[37].innerHTML == ` <p class="text-success p-2 w-50">J1</p> `) {
-        if(resultado > 4){
-            resultado = 4;
+    if(jogador == "J4"){
+        if (document.getElementById("um").innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) {
+                resultado = 1;
         }
-    }
-
-    if (document.getElementsByTagName("div")[36].innerHTML == ` <p class="text-success p-2 w-50">J1</p> `) {
-        if(resultado > 5){
-            resultado = 5;
+    
+        if (document.getElementById("dois").innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) {
+            if (resultado > 2) {
+                resultado = 2;
+            }
         }
-    }
-
-    if (document.getElementsByTagName("div")[40].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
-        if(resultado > 1){
-            resultado = 1;
+    
+        if (document.getElementById("tres").innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) {
+            if (resultado > 3) {
+                resultado = 3;
+            }
         }
-    }
-
-    if (document.getElementsByTagName("div")[39].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
-        if(resultado > 2){
-            resultado = 2;
+    
+        if (document.getElementById("quatro").innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) {
+            if (resultado > 4) {
+                resultado = 4;
+            }
         }
-    }
-
-    if (document.getElementsByTagName("div")[38].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
-        if(resultado > 3){
-            resultado = 3;
+    
+        if (document.getElementById("cinco").innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) {
+            if (resultado > 5) {
+                resultado = 5;
+            }
         }
-    }
-
-    if (document.getElementsByTagName("div")[37].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
-        if(resultado > 4){
-            resultado = 4;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[36].innerHTML == ` <p class="p-2 text-primary w-50">J2</p> `) {
-        if(resultado > 5){
-            resultado = 5;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[40].innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
-        if(resultado > 1){
-            resultado = 1;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[39].innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
-        if(resultado > 2){
-            resultado = 2;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[38].innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
-        if(resultado > 3){
-            resultado = 3;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[37].innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
-        if(resultado > 4){
-            resultado = 4;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[36].innerHTML == ` <p class="p-2 text-danger w-50">J3</p> `) {
-        if(resultado > 5){
-            resultado = 5;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[40].innerHTML == ` <p class="p-2 text-warning w-50">J4</p>`) {
-        if(resultado > 1){
-            resultado = 1;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[39].innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) {
-        if(resultado > 2){
-            resultado = 2;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[38].innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) {
-        if(resultado > 3){
-            resultado = 3;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[37].innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) {
-        if(resultado > 4){
-            resultado = 4;
-        }
-    }
-
-    if (document.getElementsByTagName("div")[36].innerHTML == ` <p class="p-2 text-warning w-50">J4</p> `) {
-        if(resultado > 5){
-            resultado = 5;
-        }
-    }
-
+    }    
+    
     if (resultado == 0) {
         resultado = resultado + 1;
     }
@@ -276,11 +293,13 @@ function dado() {
 
         pAtual1.remove();
 
-        pJ1 = (pJ1 + resultado) % tabLength;
+        pJ1 = (pJ1 + resultado);
 
-        document.getElementsByTagName("div")[pJ1 + 1].innerHTML += ` <p class="text-success p-2 w-50">J1</p> `;
+        console.log(pJ1)
 
-        if (document.getElementsByTagName("div")[pJ1 + 1].classList.contains("bg-dark")) {
+        document.getElementsByClassName("div")[pJ1].innerHTML += ` <p class="text-success p-2 w-50">J1</p> `;
+
+        if (document.getElementsByClassName("div")[pJ1].classList.contains("bg-dark")) {
             document.getElementById("jogar").disabled = true;
 
             document.body.insertAdjacentHTML("beforeend", `<div id="pergunta" class="pergunta">
@@ -326,11 +345,11 @@ function dado() {
 
         pAtual2.remove();
 
-        pJ2 = (pJ2 + resultado) % tabLength;
+        pJ2 = (pJ2 + resultado);
 
-        document.getElementsByTagName("div")[pJ2 + 1].innerHTML += ` <p class="p-2 text-primary w-50">J2</p> `;
+        document.getElementsByClassName("div")[pJ2].innerHTML += ` <p class="p-2 text-primary w-50">J2</p> `;
 
-        if (document.getElementsByTagName("div")[pJ2 + 1].classList.contains("bg-dark")) {
+        if (document.getElementsByClassName("div")[pJ2].classList.contains("bg-dark")) {
             document.getElementById("jogar").disabled = true;
 
             document.body.insertAdjacentHTML("beforeend", `<div id="pergunta" class="pergunta">
@@ -378,11 +397,11 @@ function dado() {
 
         pAtual3.remove();
 
-        pJ3 = (pJ3 + resultado) % tabLength;
+        pJ3 = (pJ3 + resultado);
 
-        document.getElementsByTagName("div")[pJ3 + 1].innerHTML += ` <p class="p-2 text-danger w-50">J3</p> `;
+        document.getElementsByClassName("div")[pJ3].innerHTML += ` <p class="p-2 text-danger w-50">J3</p> `;
 
-        if (document.getElementsByTagName("div")[pJ3 + 1].classList.contains("bg-dark")) {
+        if (document.getElementsByClassName("div")[pJ3].classList.contains("bg-dark")) {
             document.getElementById("jogar").disabled = true;
 
             document.body.insertAdjacentHTML("beforeend", `<div id="pergunta" class="pergunta">
@@ -428,12 +447,12 @@ function dado() {
 
         pAtual4.remove();
 
-        pJ4 = (pJ4 + resultado) % tabLength;
+        pJ4 = (pJ4 + resultado);
 
-        document.getElementsByTagName("div")[pJ4 + 1].innerHTML += ` <p class="p-2 text-warning w-50">J4</p> `;
+        document.getElementsByClassName("div")[pJ4].innerHTML += ` <p class="p-2 text-warning w-50">J4</p> `;
 
 
-        if (document.getElementsByTagName("div")[pJ4 + 1].classList.contains("bg-dark")) {
+        if (document.getElementsByClassName("div")[pJ4].classList.contains("bg-dark")) {
             document.getElementById("jogar").disabled = true;
 
             document.body.insertAdjacentHTML("beforeend", `<div id="pergunta" class="pergunta">
